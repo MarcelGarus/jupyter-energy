c6a98a83c9f36d55795daa79ba9e6f1993f1374c
Marcel Garus <marcel.garus@gmail.com>
2022-02-01 22:35:35 +0100

    Add MCP source and do some general code cleanup

--- python-energy/server/energy_usage.py
+++ python-energy/server/energy_usage.py
@@ -5,51 +5,104 @@ import time
 from collections import deque
 from datetime import datetime
 from math import ceil
+from re import M
 
-from measure import OngoingEnergyRecording, record_energy
+from measure import McpDevice, McpHandle, MeasureError, RaplHandle
 
 from server.utils import *
 
 
 class Source:
-    def __init__(self, id: str, name: str, recording: OngoingEnergyRecording):
+    def __init__(self, id: str, name: str):
         self.id = id
         self.name = name
-        self.recording = recording
         self.joules = 0
         self.watts = 0
+
+        # has one watts value per short_term_resolution
         self.watts_over_time = deque([])
-        # list with one joule value per long_term_resolution since server start
+
+        # has one joules value per long_term_resolution since server start
         self.long_term_joules = []
 
+    # called every short_term_resolution
+    def tick(self):
+        self.watts_over_time.append(self.watts)
+        if len(self.watts_over_time) > 100:
+            self.watts_over_time.popleft()
 
-sources = [
-    Source('all', 'all', record_energy('energy-pkg')),
-    Source('cpu', 'CPU', record_energy('energy-cores')),
-    Source('ram', 'RAM', record_energy('energy-ram')),
-    Source('gpu', 'Integrated GPU', record_energy('energy-gpu')),
-]
+        index = ceil((datetime.now() - server_started) / long_term_resolution)
+        while len(self.long_term_joules) < index - 1:
+            self.long_term_joules.append(0)
+        if len(self.long_term_joules) < index:
+            self.long_term_joules.append(self.watts)
+        else:
+            self.long_term_joules[index - 1] += self.watts
 
+    def __repr__(self):
+        return self.id
+
+
+class RaplSource(Source):
+    def __init__(self, id: str, name: str, handle: RaplHandle):
+        super().__init__(id, name)
+        self.handle = handle
+
+    def tick(self):
+        joules = self.handle.used_joules()
+        self.watts = 0 if joules == self.joules else \
+            (joules - self.joules) / short_term_resolution.total_seconds()
+        self.joules = joules
+        super().tick()
+
+class McpSource(Source):
+    def __init__(self, id: str, name: str, handle: McpHandle):
+        super().__init__(id, name)
+        self.handle = handle
+
+    def tick(self):
+        self.joules += self.watts * short_term_resolution.total_seconds()
+        self.watts = self.handle.current_watts()
+        super().tick()
+
+def discover_sources():
+    # RAPL sources
+    rapl_sources = [
+        # (internal id, user-visible name, event type)
+        ('all', 'all', 'energy-pkg'),
+        ('cpu', 'CPU', 'energy-cores'),
+        ('ram', 'RAM', 'energy-ram'),
+        ('gpu', 'Integrated GPU', 'energy-gpu'),
+    ]
+    for (id, name, event_type) in rapl_sources:
+        try:
+            yield RaplSource(id, name, RaplHandle(event_type))
+        except MeasureError:
+            pass # Event is not available on this machine.
+
+    # MCP sources
+    for device_id in range(5):
+        try:
+            device = McpDevice(f'/dev/ttyACM{device_id}')
+        except MeasureError:
+            continue # This MCP is not connected.
+        for channel in range(2):
+            yield McpSource(
+                f'mcp{device_id}ch{channel}',
+                f'MCP {device_id}, channel {channel}',
+                McpHandle(device, channel),
+            )
+
+sources = list(discover_sources())
+if len(sources) == 0:
+    print(f'No sources available. Maybe you want to run this as sudo or adjust perf_event levels?')
+    exit(0)
+else:
+    print(f'Available sources: {sources}')
 
 def tick():
     for source in sources:
-        joules = source.recording.used_joules()
-        source.watts = (joules - source.joules) / \
-            short_term_resolution.total_seconds()
-        source.joules = joules
-
-        source.watts_over_time.append(source.watts)
-        if len(source.watts_over_time) > 100:
-            source.watts_over_time.popleft()
-
-        index = ceil((datetime.now() - server_started) / long_term_resolution)
-        while len(source.long_term_joules) < index - 1:
-            source.long_term_joules.append(0)
-        if len(source.long_term_joules) < index:
-            source.long_term_joules.append(source.watts)
-        else:
-            source.long_term_joules[index - 1] += source.watts
-
+        source.tick()
 
 def monitor():
     tick_repeatedly(short_term_resolution, tick)
